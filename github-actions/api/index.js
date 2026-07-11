0590333d61fe700f8295606a6a2d7708dea4c995
b1degain <b1degian@proton.me>
2026-07-10 23:54:11 -0300

    Create minimal API

---
+++ github-actions/api/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Hello GitHub Actions 🚀'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
